0db79b9c1cc7fdaf5afe86f6cd6bf9b7bdccf8d1
David A. Desrosiers <setuid@gmail.com>
2023-10-17 11:51:26 -0400

    Cleanup and rewrite/removal of some unused code, will reintroduce later

--- lpmadison.py
+++ lpmadison.py
@@ -4,24 +4,17 @@ import argparse
 import pendulum
 from launchpadlib.launchpad import Launchpad
 
-instance = "production"
-valid_api_versions = ("beta", "1.0", "devel")
-api_version = "devel"
+INSTANCE = "production"
+VALID_API_VERSIONS = ("beta", "1.0", "devel")
+AP_VERSION = "devel"
 
 
 def parse_repo(args):
-    launchpad = Launchpad.login_anonymously("ubuntu", instance, version=api_version)
-
+    launchpad = Launchpad.login_anonymously("ubuntu", INSTANCE, version=API_VERSION)
     ubuntu = launchpad.distributions["ubuntu"]
     archive = ubuntu.main_archive
-
-    # ubuntu = launchpad.distributions["cloud-archive"]
-    # archive = ubuntu.cloud_archive
-
     series = ubuntu.getSeries(name_or_version=args.series)
 
-    #########################################################
-    # For Binary
     package_args = {"exact_match": True}
 
     if args.arch:
@@ -33,57 +26,37 @@ def parse_repo(args):
 
     pkg = archive.getPublishedBinaries(**package_args)
 
-    #########################################################
-    # For Sources
-    # package_args = {"exact_match": True, "distro_series": series}
-
-    # print(package_args)
-
-    # if args.arch:
-    #     print(package_args)
-    #     print(series.getDistroArchSeries(archtag=args.arch))
-
-    # if args.package:
-    #     package_args["source_name"] = args.package
-
-    if args.date:
-        filter_date = args.date
-
-    # pkg = archive.getPublishedSources(**package_args)
-
     for p in pkg:
-        (publish_date, delta) = parse_publish_date(args, p.date_published)
-
-        # if pendulum.parse(args.date).format("YYYY-MM-DD") == publish_date.format(
-        #     "YYYY-MM-DD"
-        # ):
+        publish_date, delta = parse_publish_date(p.date_published)
 
         if args.lineout:
-            print( f"{p.date_published} {p.source_package_name} {p.source_package_version} {p.binaryFileUrls()}")
-        else:
             print(
-                f"Package: '{p.source_package_name}'\n"
-                f"\tVersion: '{p.source_package_version}'\n"
-                f"\tPublished: '{p.date_published}'\n"
-                f"\tDays ago: {delta.in_days()}"
+                f"{p.date_published} {p.source_package_name} {p.source_package_version} {p.binaryFileUrls()}"
             )
-            # f"\tURLs: '{p.binaryFileUrls()}\n"
-            for u in p.binaryFileUrls():
-                print(f"\t{u}\n")
+        else:
+            print_package_details(p, delta)
 
 
-def parse_publish_date(args, foo):
+def parse_publish_date(date_str):
     now = pendulum.now()
-
-    publish_date = pendulum.parse(str(foo))
+    publish_date = pendulum.parse(str(date_str))
     delta = now - publish_date
-
     return publish_date, delta
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Launchpad Reposoitory Mirror Builder")
+def print_package_details(package, delta):
+    print(
+        f"Package: '{package.source_package_name}'\n"
+        f"\tVersion: '{package.source_package_version}'\n"
+        f"\tPublished: '{package.date_published}'\n"
+        f"\tDays ago: {delta.in_days()}"
+    )
+    for url in package.binaryFileUrls():
+        print(f"\t{url}\n")
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Launchpad Repository Mirror Builder")
     parser.add_argument("--series", help="Search packages in this series")
     parser.add_argument("--arch", help="Limit results to this architecture")
     parser.add_argument("--package", help="Limit search to a specific package")
@@ -91,14 +64,16 @@ def parse_args():
     parser.add_argument("--date", help="Search packages published on <date>")
     parser.add_argument("--before", help="Search packages published before <date>")
     parser.add_argument("--after", help="Search packages published after <date>")
-    parser.add_argument('--lineout', action=argparse.BooleanOptionalAction, help="Produce line-oriented output instead of the default stanza-oriented output")
-
+    parser.add_argument(
+        "--lineout",
+        action=argparse.BooleanOptionalAction,
+        help="Produce line-oriented output instead of the default stanza-oriented output",
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-
     if args.series:
         parse_repo(args)
 
